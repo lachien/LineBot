ba7fa130c0b3319b7285cb68015afcbbb74f16a9
lachien <49382666+lachien@users.noreply.github.com>
2020-06-13 17:04:21 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -57,6 +57,30 @@ def getNews():
     tmp = title + ': ' +url
     return tmp
 
+from bs4 import BeautifulSoup
+
+def movie(num):
+    num = int(num)
+    target_url = 'http://www.atmovies.com.tw/movie/next/0/'
+    print('Start parsing movie ...')
+    rs = requests.session()
+    res = rs.get(target_url, verify=False)
+    res.encoding = 'utf-8'
+    soup = BeautifulSoup(res.text, 'html.parser')
+    content = []
+    for index, data in enumerate(soup.select('ul.filmListAll a')):
+        if index == num:
+            break
+        title = data.text.replace('\t', '').replace('\r', '')
+        link = "http://www.atmovies.com.tw" + data['href']
+        if len(title) == 0:
+            num += 1
+            continue
+        content += [[title, link]]
+    return content
+
+
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     # 傳送文字
@@ -74,9 +98,36 @@ def handle_message(event):
             package_id='1',
             sticker_id='1'
         )
+        
+    elif event.message.text.startswith('電影-'):
+        _, num = event.message.text.split('-')
+        print(num)
+        movie_info = movie(num)
+        print(movie_info)
+        columns_info = []
+        for m in movie_info:
+            colum = CarouselColumn(
+                    thumbnail_image_url='https://example.com/item1.jpg',
+                    title=m[0],
+                    text=m[0],
+                    actions=[
+                        URITemplateAction(
+                            label='前往觀看',
+                            uri=m[1]
+                        )
+                    ]
+                )
+            columns_info += [colum]
+        print(columns_info)        
+        message = TemplateSendMessage(
+            alt_text='新聞文章',
+            template=CarouselTemplate(columns= columns_info)
+        )
+        
     else:
         message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
